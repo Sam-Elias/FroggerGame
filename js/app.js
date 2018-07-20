a6b80337cc4d889633c13fa4c16fb09094f04c26
Samir Elias <samirelias@gmail.com>
2018-07-20 01:20:24 -0300

    add rock.png to resources add Enemy constructor

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(enen, x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = enen;
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -29,6 +31,9 @@ const Player = function() {
 }
 // This class requires an update(), render() and
 // a handleInput() method.
+Player.prototype.update = function(dt) {
+
+}
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
